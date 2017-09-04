159beb20b20c1a4dbac6665a3e8c864a3a88a73a
Aaron Shim <aaron.k.shim@gmail.com>
2017-09-04 13:12:30 -0700

    adding extra pages

--- js/main.js
+++ js/main.js
@@ -4,21 +4,71 @@ sitemap:
   exclude: 'yes'
 ---
 
+function showBlog() {
+  $('.about').hide()
+  $('.projects').hide()
+  $('.main-post-list').show()
+}
+
+function showAbout() {
+  $('.main-post-list').hide()
+  $('.projects').hide()
+  $('.about').show()
+}
+
+function showProjects() {
+  $('.about').hide()
+  $('.main-post-list').hide()
+  $('.projects').show()
+}
+
+var buttonActions = {
+  'blog-button': showBlog,
+  'about-button': showAbout,
+  'projects-button': showProjects
+}
+
 $(document).ready(function () {
-  $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
-  })
+  for (var buttonClass in buttonActions) {
+    $('a.' + buttonClass).click(function (e) {
+      // It would be nice if we could find the function during the initial setup and then bind it...
+      // But JavaScript scoping... blegh!
+      buttonActions[e.target.className]()
+
+      if ($('.panel-cover').hasClass('panel-cover--collapsed') && window.location.hash == e.target.hash) return
+      
+      currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        // Slide in the content stuff if the page is already in a collapsed mode
+        var animationName = 'slideInRight'
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+        $('.content-wrapper').addClass('animated ' + animationName).one(animationEnd, function() {
+          $(this).removeClass('animated ' + animationName)
+        })
+      } else {
+        // No slide-in of the main content during the initial panel-cover drawing away to the left
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+        $('.panel-cover').addClass('panel-cover--collapsed') // only add this after the animation is done
+      }
+    })
+  }
+
+  // Maybe whitelist our good pages?
+  if (window.location.hash) {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
 
   if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    showBlog()
+  }
+
+  if (window.location.hash && window.location.hash == '#about') {
+    showAbout()
+  }
+
+  if (window.location.hash && window.location.hash == '#projects') {
+    showProjects()
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
